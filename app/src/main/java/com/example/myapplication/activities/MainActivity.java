a12287e2ba889ac75adb651ad35c531ffd03fce9
vishalsharma <vishal.sharma@symbl.ai>
2022-01-31 14:08:33 +0530

    Disabled dark mode in the app

--- app/src/main/java/com/example/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.myapplication.R;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(this::goToDashboard, 1000);
